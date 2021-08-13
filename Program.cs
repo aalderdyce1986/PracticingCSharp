6acebe02ced4d20fe890d4fe1651faea388cfaae
aaron <aaron@DESKTOP-OV0AOSF>
2021-08-13 13:32:09 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticingCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
